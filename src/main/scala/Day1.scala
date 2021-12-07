9d8a11e2d45c747a90a38b66a8d9f1da33a0328f
PIDAMI <pop-dod@rambler.ru>
2021-12-07 04:06:16 +0300

    day 2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,32 +1,41 @@
+
 case class Input(file:String){
   val data = {
     val source = scala.io.Source.fromFile(file)
-    try source.mkString.split("\n").toList.map(_.toInt) finally source.close()
+    try source.mkString.split("\n").toList finally source.close()
   }
 }
 
 
 object Day1 extends App{
 
-  val input = Input("day1.txt")
-  def version1 = {
-    input.data.sliding(2).count{
-      case List(a, b) => a < b
+  val data = Input("day1.txt").data
+  def version1FirstPuzzle = {
+    data.sliding(2).count{
+      case List(a, b) => a.toInt < b.toInt
       case _ => false
     }
   }
 
 
-  def version2 = {
+  def version2FirstPuzzle = {
 
-    val lines = input.data
+    val lines = data
     val zipped = for {
       ind <- lines.indices if ind != 0
-    } yield lines(ind) > lines(ind-1)
+    } yield lines(ind).toInt > lines(ind-1).toInt
     zipped.foldLeft(0)((sum,isBigger) => if (isBigger) sum + 1 else sum)
   }
 
-  println(version1)
-  println(version2)
+
+  def v1SecondPuzzle = {
+    data.sliding(4).count {
+      case List(a,_,_,d) => a.toInt < d.toInt
+      case _ => false
+    }
+  }
+
+
+
 
 }
--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -1,13 +1,40 @@
 object Day2 extends App{
 
-  val data = Input("resources/day2.txt").data
 
-  def v1 = {
-    data.sliding(4).count {
-        case List(a,_,_,d) => a < d
-        case _ => false
-    }
+  case class Position(horz:Int,depth:Int){
+    def apply(delta:Position) =
+      Position(horz + delta.horz,depth + delta.depth)
+    def toInt = horz * depth
   }
-  print(v1)
+
+  def part1 = Input("resources/day2.txt").data.map{ x =>
+    x.split(" ")(0) match {
+      case "forward" => Position(x.split(" ")(1).toInt, 0)
+      case "down" => Position(0, x.split(" ")(1).toInt)
+      case "up" => Position(0, -x.split(" ")(1).toInt)
+    }
+  }.foldLeft(Position(0,0))(_.apply(_)).toInt
+
+
+
+  def part2 = {
+    var aim = 0
+    var pos = 0
+    var depth = 0
+    Input("resources/day2.txt").data.foreach{ x =>
+      x.split(" ")(0) match {
+        case "forward" =>
+          pos += x.split(" ")(1).toInt;
+          depth += x.split(" ")(1).toInt * aim
+        case "down" => aim += 1
+        case "up" => aim -= 1
+      }
+    }
+    depth * pos
+  }
+
+  print(part2)
+
+
 
 }
